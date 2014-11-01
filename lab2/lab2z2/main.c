8baac9717dea8099ffafb0eb55bfe047f912f74b
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2014-11-01 13:54:59 +0100

    init dla z2, z3

---
+++ lab2/lab2z2/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+
+	int status;
+	int n = 2;
+	int m = 50;
+
+	int i = 0;
+
+	pid_t ch_pid, ppid, pid;
+	ch_pid = fork();
+	
+	while (1) {
+		sleep(1);
+
+		if (ch_pid != 0 && i < n) {
+			ppid = getppid();
+       		pid = getpid();
+			printf("PARENT\t%d\t%d\t%d\n", ch_pid, pid, ppid);
+		} else if (ch_pid == 0 && i < m) {
+			ppid = getppid();
+       		pid = getpid();
+			printf("CHILD\t%d\t%d\t%d\n", ch_pid, pid, ppid);
+		}
+
+		i++;
+	
+		if (i >= n && ch_pid != 0) {
+			ch_pid = wait(&status);
+			//if (WIFEXITED(status) != 0) {
+				printf("CHILD\t%d\t%d\n", ch_pid, WEXITSTATUS(status));
+				exit(0);
+			//}
+		}
+
+		if (n > m) {
+			if (i >= n) {
+				break;
+			}
+		} else {
+			if (i >= m) {
+				break;
+			}
+		}
+    }
+
+	return 0;
+}
+
