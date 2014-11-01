d01332af86b00272e6a973dd3769f3488cd1139a
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2014-11-01 13:30:11 +0100

    Program z laborek

---
+++ lab2/lab2z1/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+    int n = 6;
+    int m = 10;
+
+    int i = 0;
+
+    pid_t ch_pid, ppid, pid;
+    ch_pid = fork();
+
+    while (1) {
+        sleep(1);
+
+        if (ch_pid != 0 && i < n) {
+            ppid = getppid();
+            pid = getpid();
+            printf("PARENT\t%d\t%d\t%d\n", ch_pid, pid, ppid);
+        } else if (ch_pid == 0 && i < m) {
+            ppid = getppid();
+            pid = getpid();
+            printf("CHILD\t%d\t%d\t%d\n", ch_pid, pid, ppid);
+        }
+
+        i++;
+
+        if (i >= n && ch_pid != 0) {
+            exit(0);
+        }
+
+        if (n > m) {
+            if (i >= n) {
+                break;
+            }
+        } else {
+            if (i >= m) {
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
