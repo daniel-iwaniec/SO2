11f22b72899dfae597ab08adc0358c3bd2ce9f87
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2014-11-01 14:58:10 +0100

    init

--- lab2/lab2z3/main.c
+++ lab2/lab2z3/main.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/unistd.h>
 #include <sys/wait.h>
+#include <sys/unistd.h>
 
-int main() {
-    //code
+int main(int argc, char** argv) {
+    int n = argv[1];
+    int i = 0;
+    
+    pid_t ch_pid, ppid, pid;
+    
+    ch_pid = fork();
+    
+    while (1) {
+        sleep(1);
+
+        if (ch_pid != 0) {
+            ppid = getppid();
+            pid = getpid();
+            printf("PARENT\t%d\t%d\t%d\n", ch_pid, pid, ppid);
+        } else if (ch_pid == 0) {
+            ppid = getppid();
+            pid = getpid();
+            printf("CHILD\t%d\t%d\t%d\n", ch_pid, pid, ppid);
+        }
+
+        i++;
+    }    
 
     return EXIT_SUCCESS;
 }
