fa80a6db93ef841579160990aae6e7722f63f38e
Daniel Iwaniec <daniel.iwaniec92@gmail.com>
2014-11-02 12:21:56 +0100

    z5 prosty init

--- lab2/lab2z5/main.c
+++ lab2/lab2z5/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
+    pid_t childPid;
+    int firstRun = 1;
+    char command[100];
+
+    while (1) {
+        if (!firstRun) {
+            printf("\n");
+        } else {
+            firstRun = 0;
+        }
+        printf("COMMAND: ");
+
+        scanf("%s", command);
+
+        childPid = fork();
+
+        if (childPid != 0) {
+            childPid = wait();
+            if (strcmp(command, "exit") == 0) {
+                exit(EXIT_SUCCESS);
+            }
+        } else {
+            printf("%s", command);
+            exit(EXIT_SUCCESS);
+        }
+    }
+
     return EXIT_SUCCESS;
 }
